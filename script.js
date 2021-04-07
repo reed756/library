b6d1f487e55d3634cfe48886784d574d32bc75e3
reed756 <68922323+reed756@users.noreply.github.com>
2021-04-07 08:01:06 +0100

    added function that loops through array and adds the books to the list

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book(title, author, number, read) {
     this.title = title
     this.author = author
@@ -8,6 +10,25 @@ function Book(title, author, number, read) {
     }
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295 pages', 'not read yet');
+function addBookToLibrary(book) {
+    myLibrary.push(book.info());
+}
 
-console.log(theHobbit.info());
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295 pages', 'not read yet');
+const greatGatsby = new Book('The Great Gatsby', 'F. Scott Fitzgerald', '215 pages', 'not read yet');
+const annaKarenina = new Book('Anna Karenina', 'Leo Tolstoy', '864 pages', 'not read yet');
+
+addBookToLibrary(theHobbit);
+addBookToLibrary(greatGatsby);
+addBookToLibrary(annaKarenina);
+
+let content = document.querySelector(".content");
+let booknew = document.querySelector("#new");
+
+function addBooksToList() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        let card = document.createElement('div');
+        card.textContent = `${myLibrary[i]}`;
+        content.appendChild(card);
+    }
+}
